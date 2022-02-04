7fff904383a942892007b5bfd72c00308fc5a951
GongZone <dnjsqhwo@gmail.com>
2022-02-04 23:01:25 +0900

    update

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,15 @@
 import Register from "./page/Register";
 import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
 import Login from "./page/Login";
-
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
