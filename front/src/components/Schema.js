f807b7615e19f0195b1b307fc947c5fe04cd5c44
frontjsw <89008053+frontjsw@users.noreply.github.com>
2022-02-04 22:46:58 +0900

    로그인 페이지 수정

--- front/src/components/Schema.js
+++ front/src/components/Schema.js
@@ -1,5 +1,16 @@
 import * as yup from "yup";
 
+const loginSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required("필수 입력 항목입니다.")
+    .min(6, "최소 6자리의 아이디를 입력해주세요."),
+  password: yup
+    .string()
+    .required("필수 입력 항목입니다.")
+    .min(6, "최소 6자리의 비밀번호를 입력해주세요."),
+});
+
 const signupSchema = yup.object().shape({
   username: yup
     .string()
@@ -24,4 +35,4 @@ const signupSchema = yup.object().shape({
     .min(6, "최소 1자리의 나이를 입력해주세요."),
 });
 
-export default signupSchema;
+export { loginSchema, signupSchema };
