406e00984ff4599ca496a8fe3d3b20dd674f6349
GongZone <dnjsqhwo@gmail.com>
2022-02-04 22:59:19 +0900

    update

--- front/src/App.js
+++ front/src/App.js
@@ -1,3 +1,7 @@
+import Register from "./page/Register";
+import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
+import Login from "./page/Login";
+
 function App() {
   return <></>;
 }
